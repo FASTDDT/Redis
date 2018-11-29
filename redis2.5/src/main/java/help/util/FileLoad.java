febb5d7a2a962a1d03d0328717ca2648fc7ffe87
fastddt <280923164@qq.com>
2018-11-29 23:40:01 +0800

    no message

--- redis2.5/src/main/java/help/util/FileLoad.java
+++ redis2.5/src/main/java/help/util/FileLoad.java
@@ -3,6 +3,7 @@ package help.util;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -10,9 +11,15 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
+@Component
 @Slf4j
 public class FileLoad {
-    private static String basePath="D:\\upload\\";
+    private static String basePath;
+
+    @Value("${upload.path}")
+    public void setBasePath(String path){
+        FileLoad.basePath=path;
+    }
     public static void upLoadFile(MultipartFile mfile) throws IOException {
         String originalFilename=mfile.getOriginalFilename();
         String uuid= UUID.randomUUID().toString();
@@ -22,12 +29,13 @@ public class FileLoad {
         LocalDateTime dateTime = LocalDateTime.now();
         String time = dateTime.format(formatter); // "1986-04-08 12:30"
         String path=basePath+time;
-        System.out.println("path:"+path);
+
         File file=new File(path);
         if (!file.exists()){
             file.mkdirs();
         }
         path+="\\"+uuid+suffix;
+        System.out.println("path:"+path);
         mfile.transferTo(new File(path));
     }
 }
